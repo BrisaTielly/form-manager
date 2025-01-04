201a5cc5969e59a8553d7a546acb82f37c3d53fc
Brisa Tielly <Brisatielly12@gmail.com>
2025-01-03 23:02:31 -0300

    Cadastro do primeiro usuário

--- src/App.java
+++ src/App.java
@@ -1,27 +1,39 @@
+import entities.User;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        List<String> questions = new ArrayList<>();
+        List<String> staticQuestions = new ArrayList<>();
         String path = ".\\formulario.txt";
         
         try(BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
             while(line != null){
-            System.out.println(line);
-            questions.add(line);
+            staticQuestions.add(line);
             line = br.readLine();
             }
             
         }catch(IOException e){
             System.out.println("Error " + e.getMessage());
         }
+
+        String[] answersFields = new String[4];
+        int i = 0;
+        for(String question : staticQuestions){
+            System.out.println(question);
+            answersFields[i++] = sc.nextLine();
+        }
+        User user = new User(answersFields[0], answersFields[1], Integer.parseInt(answersFields[2]), Double.parseDouble(answersFields[3]));
+        System.out.println(user);
         sc.close();
     }
 }
